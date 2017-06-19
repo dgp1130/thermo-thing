3bafc7c3fd828783e010cd0ca435225f3b489ada
Douglas Parker <dgp1130422@gmail.com>
2017-06-18 19:41:17 -0700

    Adding Spark dependency and Hello World example.

--- app/src/main/java/com/douglasgparker/thermothing/ThermoActivity.kt
+++ app/src/main/java/com/douglasgparker/thermothing/ThermoActivity.kt
@@ -3,16 +3,25 @@ package com.douglasgparker.thermothing
 import android.app.Activity
 import android.os.Bundle
 import android.util.Log
+import spark.Spark
+import spark.Spark.*
 
 /**
  * Main Activity class for the project.
  */
 class ThermoActivity : Activity() {
-    val TAG: String = "Thermo"
+    val TAG = "Thermo"
+    val PORT = 8000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Log.v(TAG, "Hello World!")
+        Log.v(TAG, "Initializing...")
+
+        // Initialize server
+        Spark.port(PORT)
+        get("/hello") { _, _ -> "Hello World!" }
+
+        Log.v(TAG, "Serving on port $PORT...")
     }
 }
